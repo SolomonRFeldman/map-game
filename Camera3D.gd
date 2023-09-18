aff07ba5a154e73c2498a35919a64fe02ac4c613
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2023-09-17 23:03:15 -0400

    more accurately defines camera_displacement

--- Camera3D.gd
+++ Camera3D.gd
@@ -20,7 +20,7 @@ func _input(event):
 	transform.basis = Basis() # reset rotation
 	var cam_zoom = -(-400 - position.z)
 	var cam_angle = (cam_zoom / 180) * PI/2
-	var camera_displacement = tan(cam_angle) * (-cam_zoom + 100)
-	position.y = -camera_displacement
+	var camera_displacement = tan(cam_angle) * (cam_zoom - 100)
+	position.y = camera_displacement
 	rotate_object_local(Vector3(0, 1, 0), PI)
 	rotate_object_local(Vector3(1, 0, 0), cam_angle)
