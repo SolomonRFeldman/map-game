57ba1483acf37fa52449bc4e90ff3a99fccd38fd
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2023-09-17 22:41:29 -0400

    sets longitude wrap around to [-180,180] instead of [0,360]
    This mimics real world coordinates better

--- Ground.gd
+++ Ground.gd
@@ -13,9 +13,11 @@ var lon = -74.00597
 func _process(delta):
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var new_lat = lat - (input_dir.y / 10)
-	lon = fmod(lon + (input_dir.x / 10), 360.0)
-	if lon < 0:
+	lon = lon + (input_dir.x / 10)
+	if lon < -180:
 		lon += 360
+	if lon > 180:
+		lon -= 360
 	if(new_lat <= 90 && new_lat >= -90):
 		lat = new_lat
 	var lon_rads = (lon / 90) * -PI/2
@@ -23,3 +25,4 @@ func _process(delta):
 	transform.basis = Basis() # reset rotation
 	rotate_object_local(Vector3(0, 1, 0), lon_rads) # first rotate in Y
 	rotate_object_local(Vector3(cos(lon_rads), 0, sin(lon_rads)), lat_rads)
+	print(lon)
